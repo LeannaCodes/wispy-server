6df30ddad584d5bcee80d7a16131f4bd491b7df4
LEANNA LUCAS <LEANNALUCAS10@GMAIL.COM>
2022-08-04 16:41:10 +0100

    add server to netlify

--- src/api.js
+++ src/api.js
@@ -1,31 +1,36 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const cors = require("cors");
 require("dotenv").config();
 const axios = require("axios");
 
 const app = express();
+const router = express.Router();
 
 app.use(cors());
 
 const PORT = process.env.PORT || 3005;
 
-app.get("/", (request, response) => {
+router.get("/", (request, response) => {
   console.log("hello world");
   response.status(200).send("Hello World");
 });
 
-app.get("/condition/:name", async (request, response) => {
+router.get("/condition/:name", async (request, response) => {
   const { name } = request.params;
 
   const API = `https://api.nhs.uk/mental-health/conditions/${name}?subscription-key=${process.env.REACT_APP_SECONDARY_KEY}`;
   try {
     const result = await axios.get(API);
-
+    // console.log(result.data.hasPart);
     response.status(200).send(result.data);
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     response.status(500).send(error);
   }
 });
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.use(`/.netlify/functions/api`, router);
+
+module.exports.handler = serverless(app);
+// old way: app.listen(PORT, () => console.log(`listening on port ${PORT}`));
